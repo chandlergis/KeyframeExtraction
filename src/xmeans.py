3f8e1c1ef610156b5a830fe36834dc13d6117c81
huangyihao <huangyihao@smarthink.cc>
2024-06-18 11:52:24 +0800

    add image

--- src/xmeans.py
+++ src/xmeans.py
@@ -1,3 +1,256 @@
+有什么可以帮你的吗
+
+import cv2
+import argparse
+import numpy as np
+
+from utils import bundleImage
+from pyclustering.cluster.xmeans import xmeans
+from pyclustering.cluster.center_initializer import kmeans_plusplus_initializer
+
+def main(args):
+cap = cv2.VideoCapture(args.filepath)
+
+frameWidth = int(cap.get(3))
+frameHeight = int(cap.get(4))
+newHeight = int(frameHeight / 5)
+newWidth = int(frameWidth / 5)
+framesAveBGR = []
+
+while True:
+    ret, curFrame = cap.read()
+
+    if not ret:
+        break
+    
+    aveBGR = 0
+    for channelNum in range(3):
+        # BGRの各チャンネルのヒストグラムを計算
+        hist = cv2.calcHist([curFrame], channels=[channelNum], mask=None, histSize=[256], ranges=[0, 256])
+        
+        # 正規化
+        hist = cv2.normalize(hist, hist)
+
+        # 全てのビンの平均
+        colorSum = 0
+        for value in hist:
+            colorSum += value[0]    # value = [0.008...], value[0] = 0.008...
+        colorSum /= 256
+
+        # 平均を計算するために加算
+        aveBGR += colorSum
+
+    # 平均を計算
+    aveBGR /= 3
+
+    # 記録
+    framesAveBGR.append(aveBGR * 100)
+    
+cv2.destroyAllWindows()
+
+# x-means設定
+framesAveBGR = np.array(framesAveBGR)   # (111,)
+framesAveBGR = framesAveBGR.reshape(-1, 1)  # (111, 1)
+xm_c = kmeans_plusplus_initializer(framesAveBGR, args.k).initialize()    # クラスタ数の初期値を設定
+xm_i = xmeans(data=framesAveBGR , initial_centers=xm_c, kmax=20, tolerance=args.tolerance, core=True) # kmax: クラスタ最大数
+# x-means実行
+xm_i.process()
+
+# ラベルと中心を取得    グループは0開始
+labels = np.ones(framesAveBGR.shape[0])
+for k in range(len(xm_i._xmeans__clusters)):
+    labels[xm_i._xmeans__clusters[k]] = k
+centers = np.array(xm_i._xmeans__centers)
+
+print("クラスタ数：", len(xm_i._xmeans__clusters))
+
+# クラスタ中心に最も近いデータ(= キーフレーム)のインデックスを取得
+closest = [0] * len(centers)
+minDistance = [999] * len(centers)
+for frameNum in range(len(labels)):
+    labelNum = int(labels[frameNum])
+    distance = abs(framesAveBGR[frameNum] - centers[labelNum])
+    if minDistance[labelNum] > distance:
+        minDistance[labelNum] = distance
+        closest[labelNum] = frameNum
+closest.sort()
+
+# # キーフレームの画像情報を取得
+keyframes = []
+for keyframeIdx in closest:
+    cap.set(cv2.CAP_PROP_POS_FRAMES, keyframeIdx)
+    _, keyframe = cap.read()
+    keyframes.append(cv2.resize(keyframe, (newWidth, newHeight), interpolation = cv2.INTER_LINEAR))
+cap.release()
+
+# # 描画
+resultImage = bundleImage(keyframes, newHeight, newWidth, numCols = 7)
+cv2.imshow("Frames", resultImage)
+cv2.waitKey()
+if name == 'main':
+parser = argparse.ArgumentParser(description = 'k-means')
+parser.add_argument('--filepath', default = './assets/sample.mp4', type = str, help='ファイルパス名')
+parser.add_argument('--k', default = 1, type = int, help='クラスタ数初期値')
+parser.add_argument('--tolerance', default = 1e-4, type = float, help='収束と判定するためのtolerance(許容誤差)') # 0.025
+args = parser.parse_args()
+main(args) 这段代码能在linux上使用么，我安装好了环境，然后我想提取视频的关键帧
+
+这段代码是用于从视频中提取关键帧的。它首先计算每个帧的平均颜色，然后使用x-means聚类算法将这些帧分组。每个组的中心帧被选为关键帧。这段代码应该能在已经安装了所有必要库的Linux系统上运行。
+
+这段代码依赖于以下Python库：
+
+OpenCV (cv2): 用于处理视频和图像。
+argparse: 用于处理命令行参数。
+numpy: 用于数学和矩阵操作。
+pyclustering: 用于x-means聚类。
+在运行这段代码之前，你需要确保已经在你的Linux系统上安装了这些库。你可以使用pip来安装它们：
+
+pip install opencv-python
+pip install argparse
+pip install numpy
+pip install pyclustering
+另外，这段代码中的bundleImage函数来自utils模块，但代码中并没有提供这个模块。你需要确保这个函数可以在你的环境中调用，或者你需要自己实现这个函数。
+
+最后，你需要使用正确的命令行参数来运行这个脚本。例如，如果你的视频文件位于/path/to/your/video.mp4，你可以使用以下命令来运行这个脚本：
+
+python your_script.py --filepath /path/to/your/video.mp4 --k 2 --tolerance 0.001
+这里，--k参数是x-means聚类的初始簇数，--tolerance参数是算法的收敛容忍度。你可以根据你的需要调整这些参数。
+
+如何保存提取的图片呢？
+
+你可以使用 OpenCV 的 cv2.imwrite 函数来保存图像。在你的代码中，你可以在显示结果图像之后添加一行来保存它。例如，如果你想将结果图像保存为 "result.jpg"，你可以这样做：
+
+# # 描画
+resultImage = bundleImage(keyframes, newHeight, newWidth, numCols = 7)
+cv2.imshow("Frames", resultImage)
+
+# 保存图片
+cv2.imwrite('result.jpg', resultImage)
+
+cv2.waitKey()
+在这个例子中，cv2.imwrite 函数将 resultImage 保存为一个名为 "result.jpg" 的文件。你可以根据需要更改文件名和路径。注意，cv2.imwrite 函数会自动选择适当的文件格式基于给定的文件扩展名。例如，如果你想保存为 PNG 格式，你可以将文件名更改为 "result.png"。
+
+给我修改后的全部代码
+
+好的，以下是修改后的全部代码：
+
+import cv2
+import argparse
+import numpy as np
+
+from utils import bundleImage
+from pyclustering.cluster.xmeans import xmeans
+from pyclustering.cluster.center_initializer import kmeans_plusplus_initializer
+
+def main(args):
+    cap = cv2.VideoCapture(args.filepath)
+
+    frameWidth = int(cap.get(3))
+    frameHeight = int(cap.get(4))
+    newHeight = int(frameHeight / 5)
+    newWidth = int(frameWidth / 5)
+    framesAveBGR = []
+
+    while True:
+        ret, curFrame = cap.read()
+
+        if not ret:
+            break
+        
+        aveBGR = 0
+        for channelNum in range(3):
+            hist = cv2.calcHist([curFrame], channels=[channelNum], mask=None, histSize=[256], ranges=[0, 256])
+            hist = cv2.normalize(hist, hist)
+            colorSum = 0
+            for value in hist:
+                colorSum += value[0]
+            colorSum /= 256
+            aveBGR += colorSum
+
+        aveBGR /= 3
+        framesAveBGR.append(aveBGR * 100)
+        
+    cv2.destroyAllWindows()
+
+    framesAveBGR = np.array(framesAveBGR)
+    framesAveBGR = framesAveBGR.reshape(-1, 1)
+    xm_c = kmeans_plusplus_initializer(framesAveBGR, args.k).initialize()
+    xm_i = xmeans(data=framesAveBGR , initial_centers=xm_c, kmax=20, tolerance=args.tolerance, core=True)
+    xm_i.process()
+
+    labels = np.ones(framesAveBGR.shape[0])
+    for k in range(len(xm_i._xmeans__clusters)):
+        labels[xm_i._xmeans__clusters[k]] = k
+    centers = np.array(xm_i._xmeans__centers)
+
+    print("クラスタ数：", len(xm_i._xmeans__clusters))
+
+    closest = [0] * len(centers)
+    minDistance = [999] * len(centers)
+    for frameNum in range(len(labels)):
+        labelNum = int(labels[frameNum])
+        distance = abs(framesAveBGR[frameNum] - centers[labelNum])
+        if minDistance[labelNum] > distance:
+            minDistance[labelNum] = distance
+            closest[labelNum] = frameNum
+    closest.sort()
+
+    keyframes = []
+    for keyframeIdx in closest:
+        cap.set(cv2.CAP_PROP_POS_FRAMES, keyframeIdx)
+        _, keyframe = cap.read()
+        keyframes.append(cv2.resize(keyframe, (newWidth, newHeight), interpolation = cv2.INTER_LINEAR))
+    cap.release()
+
+    resultImage = bundleImage(keyframes, newHeight, newWidth, numCols = 7)
+    
+    # 保存图片
+    cv2.imwrite('result.jpg', resultImage)
+
+    cv2.imshow("Frames", resultImage)
+    cv2.waitKey()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description = 'k-means')
+    parser.add_argument('--filepath', default = './assets/sample.mp4', type = str, help='ファイルパス名')
+    parser.add_argument('--k', default = 1, type = int, help='クラスタ数初期値')
+    parser.add_argument('--tolerance', default = 1e-4, type = float, help='収束と判定するためのtolerance(許容誤差)')
+    args = parser.parse_args()
+    main(args)
+这段代码将保存关键帧图片为 'result.jpg'。如果你想要改变保存的文件名或者路径，你可以修改 cv2.imwrite('result.jpg', resultImage) 这行代码中的 'result.jpg' 为你想要的文件名或者路径。
+
+(video_edit) ubuntu@cronus:~/Project/yihao/KeyframeExtraction/src$ python xmeans.py --filepath /home/ubuntu/Project/yihao/KeyframeExtraction/assets/test.mp4 --k 1 --tolerance 1e-4
+クラスタ数： 1
+qt.qpa.xcb: could not connect to display
+qt.qpa.plugin: Could not load the Qt platform plugin "xcb" in "/home/ubuntu/.conda/envs/video_edit/lib/python3.9/site-packages/cv2/qt/plugins" even though it was found.
+This application failed to start because no Qt platform plugin could be initialized. Reinstalling the application may fix this problem.
+
+Available platform plugins are: xcb.
+
+Aborted (core dumped)
+
+你正在尝试在没有图形用户界面（GUI）的环境下运行一个需要显示图像的程序，所以遇到了这个问题。在你的代码中，cv2.imshow 是一个需要 GUI 的函数，但是你的环境可能是一个命令行界面，比如 SSH 连接或者 Docker 容器。
+
+如果你只是想保存图片而无需显示它，你可以直接删除或注释掉 cv2.imshow 和 cv2.waitKey 这两行代码。它们的主要目的是在屏幕上显示图像，如果你不需要这个功能，就可以直接删除或注释掉它们。
+
+但是如果你确实需要在没有 GUI 的环境下显示图像，你可以考虑使用 matplotlib 库来代替 cv2.imshow。matplotlib 可以将图像保存为文件，也可以在没有 GUI 的环境下显示图像。
+
+以下是一个使用 matplotlib 显示图像的例子：
+
+import matplotlib.pyplot as plt
+
+# 显示图片
+plt.imshow(resultImage)
+plt.show()
+
+# 保存图片
+plt.imsave('result.jpg', resultImage)
+在这个例子中，plt.imshow 用来显示图像，plt.show 用来弹出显示窗口，plt.imsave 用来保存图像。注意，matplotlib 默认使用 RGB 颜色空间，而 cv2 使用 BGR 颜色空间，所以在使用 plt.imshow 前，你可能需要将图像从 BGR 转换为 RGB。
+
+帮我注释掉
+
+当然，以下是修改后的代码，我已经将 cv2.imshow 和 cv2.waitKey 这两行代码注释掉了：
+
 import cv2
 import argparse
 import numpy as np
@@ -81,3 +334,167 @@ if __name__ == '__main__':
     parser.add_argument('--tolerance', default = 1e-4, type = float, help='収束と判定するためのtolerance(許容誤差)')
     args = parser.parse_args()
     main(args)
+现在，这段代码将只保存关键帧图片，而不会尝试显示它。
+
+import cv2
+import argparse
+import numpy as np
+
+from utils import bundleImage
+from pyclustering.cluster.xmeans import xmeans
+from pyclustering.cluster.center_initializer import kmeans_plusplus_initializer
+
+def main(args):
+cap = cv2.VideoCapture(args.filepath)
+
+frameWidth = int(cap.get(3))
+frameHeight = int(cap.get(4))
+newHeight = int(frameHeight / 5)
+newWidth = int(frameWidth / 5)
+framesAveBGR = []
+
+while True:
+    ret, curFrame = cap.read()
+
+    if not ret:
+        break
+    
+    aveBGR = 0
+    for channelNum in range(3):
+        hist = cv2.calcHist([curFrame], channels=[channelNum], mask=None, histSize=[256], ranges=[0, 256])
+        hist = cv2.normalize(hist, hist)
+        colorSum = 0
+        for value in hist:
+            colorSum += value[0]
+        colorSum /= 256
+        aveBGR += colorSum
+
+    aveBGR /= 3
+    framesAveBGR.append(aveBGR * 100)
+    
+cv2.destroyAllWindows()
+
+framesAveBGR = np.array(framesAveBGR)
+framesAveBGR = framesAveBGR.reshape(-1, 1)
+xm_c = kmeans_plusplus_initializer(framesAveBGR, args.k).initialize()
+xm_i = xmeans(data=framesAveBGR , initial_centers=xm_c, kmax=20, tolerance=args.tolerance, core=True)
+xm_i.process()
+
+labels = np.ones(framesAveBGR.shape[0])
+for k in range(len(xm_i._xmeans__clusters)):
+    labels[xm_i._xmeans__clusters[k]] = k
+centers = np.array(xm_i._xmeans__centers)
+
+print("クラスタ数：", len(xm_i._xmeans__clusters))
+
+closest = [0] * len(centers)
+minDistance = [999] * len(centers)
+for frameNum in range(len(labels)):
+    labelNum = int(labels[frameNum])
+    distance = abs(framesAveBGR[frameNum] - centers[labelNum])
+    if minDistance[labelNum] > distance:
+        minDistance[labelNum] = distance
+        closest[labelNum] = frameNum
+closest.sort()
+
+keyframes = []
+for keyframeIdx in closest:
+    cap.set(cv2.CAP_PROP_POS_FRAMES, keyframeIdx)
+    _, keyframe = cap.read()
+    keyframes.append(cv2.resize(keyframe, (newWidth, newHeight), interpolation = cv2.INTER_LINEAR))
+cap.release()
+
+resultImage = bundleImage(keyframes, newHeight, newWidth, numCols = 7)
+
+# 保存图片
+cv2.imwrite('result.jpg', resultImage)
+
+# cv2.imshow("Frames", resultImage)
+# cv2.waitKey()
+if name == 'main':
+parser = argparse.ArgumentParser(description = 'k-means')
+parser.add_argument('--filepath', default = './assets/sample.mp4', type = str, help='ファイルパス名')
+parser.add_argument('--k', default = 1, type = int, help='クラスタ数初期値')
+parser.add_argument('--tolerance', default = 1e-4, type = float, help='収束と判定するためのtolerance(許容誤差)')
+args = parser.parse_args()
+main(args) 我希望能单独提取出视频的关键帧，return所有的img
+
+好的，我理解你的需求。这是修改后的代码，它将返回一个包含所有关键帧的列表：
+
+import cv2
+import argparse
+import numpy as np
+
+from utils import bundleImage
+from pyclustering.cluster.xmeans import xmeans
+from pyclustering.cluster.center_initializer import kmeans_plusplus_initializer
+
+def main(args):
+    cap = cv2.VideoCapture(args.filepath)
+
+    frameWidth = int(cap.get(3))
+    frameHeight = int(cap.get(4))
+    newHeight = int(frameHeight / 5)
+    newWidth = int(frameWidth / 5)
+    framesAveBGR = []
+
+    while True:
+        ret, curFrame = cap.read()
+
+        if not ret:
+            break
+        
+        aveBGR = 0
+        for channelNum in range(3):
+            hist = cv2.calcHist([curFrame], channels=[channelNum], mask=None, histSize=[256], ranges=[0, 256])
+            hist = cv2.normalize(hist, hist)
+            colorSum = 0
+            for value in hist:
+                colorSum += value[0]
+            colorSum /= 256
+            aveBGR += colorSum
+
+        aveBGR /= 3
+        framesAveBGR.append(aveBGR * 100)
+        
+    cv2.destroyAllWindows()
+
+    framesAveBGR = np.array(framesAveBGR)
+    framesAveBGR = framesAveBGR.reshape(-1, 1)
+    xm_c = kmeans_plusplus_initializer(framesAveBGR, args.k).initialize()
+    xm_i = xmeans(data=framesAveBGR , initial_centers=xm_c, kmax=20, tolerance=args.tolerance, core=True)
+    xm_i.process()
+
+    labels = np.ones(framesAveBGR.shape[0])
+    for k in range(len(xm_i._xmeans__clusters)):
+        labels[xm_i._xmeans__clusters[k]] = k
+    centers = np.array(xm_i._xmeans__centers)
+
+    print("クラスタ数：", len(xm_i._xmeans__clusters))
+
+    closest = [0] * len(centers)
+    minDistance = [999] * len(centers)
+    for frameNum in range(len(labels)):
+        labelNum = int(labels[frameNum])
+        distance = abs(framesAveBGR[frameNum] - centers[labelNum])
+        if minDistance[labelNum] > distance:
+            minDistance[labelNum] = distance
+            closest[labelNum] = frameNum
+    closest.sort()
+
+    keyframes = []
+    for keyframeIdx in closest:
+        cap.set(cv2.CAP_PROP_POS_FRAMES, keyframeIdx)
+        _, keyframe = cap.read()
+        keyframes.append(cv2.resize(keyframe, (newWidth, newHeight), interpolation = cv2.INTER_LINEAR))
+    cap.release()
+
+    return keyframes
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description = 'k-means')
+    parser.add_argument('--filepath', default = './assets/sample.mp4', type = str, help='ファイルパス名')
+    parser.add_argument('--k', default = 1, type = int, help='クラスタ数初期値')
+    parser.add_argument('--tolerance', default = 1e-4, type = float, help='収束と判定するためのtolerance(許容誤差)')
+    args = parser.parse_args()
+    keyframes = main(args)
